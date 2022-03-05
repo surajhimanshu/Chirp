d2b1dd49e809ea75aad9d7ada18c7e29fde37113
Abhishek <abhiaro27@gmail.com>
2022-03-05 23:14:11 +0530

    merged store

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from "../features/DataApi/posts.slice";
 import usersReducer from "../features/DataApi/users.slice";
 import userReducer from "../features/Auth/auth.slice";
+import profileReducer from "../features/Profile/profile.slice"
 export const store = configureStore({
   reducer: {
     posts: postsReducer,
     users: usersReducer,
     user: userReducer,
+    profile: profileReducer,
   },
 });
--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import profileReducer from "./Components/Profile/profile.slice.js";
-
-
-export const store = configureStore({
-    reducer:{
-        profile:profileReducer,
-    }
-})
