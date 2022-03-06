ef81f34c180927d4d846c441e49b2569e0e2f6ed
Guptaji03 <saguptaji1998@gmail.com>
2022-03-06 12:01:10 +0530

    signup page changes

---
+++ src/features/Setting&Privacy/Setting.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Setting = () => {
+ let pages=["Your account","Twitter Blue","Security and accouont access","Privacy and safety","Notification","Accessibility,"]
+ console.log(pages)
+  return (
+    <div>
+       
+
+    </div>
+  )
+}
+
+export default Setting
